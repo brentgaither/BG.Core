247f4860c2a01edd6d37dabe7813dd3ee528ce43
Brent Gaither <brent.w.gaither@gmail.com>
2016-07-04 12:42:52 -0600

    initial commit

---
+++ BG.Core.Guard/Guard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BG.Core
+{
+    /// <summary>
+    /// Guards against an object from being null
+    /// </summary>
+
+    public static class Guard
+    {
+        /// <summary>
+        /// Throws arguement exception if string is  null or empty
+        /// </summary>
+        /// <param name="checkString"></param>
+        /// <param name="message"></param>
+        public static void NotNullOrEmpty(string checkString, string message = "String cannot be null or empty.")
+        {
+            if (string.IsNullOrEmpty(checkString))
+            {
+                throw new ArgumentException(message);
+            }
+            return;
+        }
+        /// <summary>
+        /// Throws argument null exception if object is null
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="message"></param>
+        public static void NotNull(object obj, string message = "Paramter cannot be null")
+        {
+            if (obj != null) return;
+
+            throw new ArgumentNullException(null, message);
+        }
+    }
+
+}
---
+++ BG.Core.GuardTest/GuardTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using BG.Core;
+
+namespace BG.Core.GuardTest
+{
+
+    [TestFixture]
+    public class GuardTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NotNullOrEmpty_WithEmptyString_ThrowsArgumentException()
+        {
+            var paramterTest = string.Empty;
+           
+               Guard.NotNullOrEmpty(paramterTest);
+
+        }
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NotNullOrEmpty_With_Null_ThrowsArgumentException()
+        {
+            string paramterTest = null;
+            Guard.NotNullOrEmpty(paramterTest);
+        }
+
+        [Test]
+        public void NotNullOrEmpty_WithValidString_Passes()
+        {
+            var paramterTest = "test";
+            Guard.NotNullOrEmpty(paramterTest);
+        }
+        
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NotNull_WithNull_ThrowsArgumentNullException()
+        {
+            object testObject = null;
+            Guard.NotNull(testObject);
+        }
+
+        [Test]
+        public void NotNull_WithObject_Passes()
+        {
+            object testObject = new object();
+            Guard.NotNull(testObject);
+        }
+    }
+}
